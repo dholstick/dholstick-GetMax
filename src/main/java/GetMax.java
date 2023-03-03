360ec3473e393ae673961c159220fe8940468099
dholstick <dantonioholstick00@gmail.com>
2023-03-03 21:23:28 +0000

    Completed Get Max Lab

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxNum = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > maxNum){
+                maxNum = arr[i];
+            }
+        }
+        return maxNum;
     }
 }
